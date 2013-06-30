724614b5d2624d57cfb5fef7f3c8f4ba2e447853
pkjain <Parveen Kumar>
2013-06-30 09:45:07 +0000

    initial code

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 wp-angularjs-control-include
 ============================
 
-Wordpress plugin - include angularjs javascript for a specific post (using custom field)
+
+# Wordpress plugin description
+Include angularjs javascript for a specific post (using custom field angularjs-include)
+
+It is a wordpress plugin to include angularjs javascript file in wordpress posts or pages if custom field angularjs-include is set to true.
+
+# Author
+[infoheap](http://infoheap.com/)
+
+# License
+* [GPL](http://www.gnu.org/licenses/gpl-2.0.html)
---
+++ wp-angularjs-control-include.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: WP angularjs control include
+Plugin URI: http://infoheap.com/
+Description: Include angularjs javascript for a specific post (using custom field angularjs-include)
+Author: infoheap team
+Author URI: http://infoheap.com/
+Version: 1.0
+*/
+
+
+function waci_enqueue_js () {
+  global $post;
+  $post_angularjs_include_value = get_post_meta($post->ID, 'angularjs-include', true);
+  if (in_array($post_angularjs_include_value, array('true', '1')) ) {
+    wp_enqueue_script('angularjs-js', plugins_url('js/angular.js', __FILE__), null, null, true);
+  }
+}
+add_action('wp_enqueue_scripts', 'waci_enqueue_js_css');
+
+?>
