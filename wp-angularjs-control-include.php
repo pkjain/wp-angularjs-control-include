73d3544f74847adb2584a9a3b427e1a9b15ac716
pkjain <Parveen Kumar>
2013-06-30 10:48:23 +0000

    commnt

--- wp-angularjs-control-include.php
+++ wp-angularjs-control-include.php
@@ -13,7 +13,7 @@ function waci_enqueue_js () {
   global $post;
   $post_angularjs_include_value = get_post_meta($post->ID, 'wp-angularjs-include', true);
   if (in_array($post_angularjs_include_value, array('true', '1')) ) {
-    wp_enqueue_script('angularjs-js', plugins_url('js/angular.js', __FILE__), null, null, true);
+    wp_enqueue_script('angularjs-js', plugins_url('js/angular.js', __FILE__), null, null, true/*in_footer*/);
   }
 }
 add_action('wp_enqueue_scripts', 'waci_enqueue_js');
