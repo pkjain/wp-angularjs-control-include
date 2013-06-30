b07a1267aa2d0ed19c6eca069570c4bcb92a902e
pkjain <Parveen Kumar>
2013-06-30 09:47:23 +0000

    wp-angularjs-include

--- wp-angularjs-control-include.php
+++ wp-angularjs-control-include.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP angularjs control include
 Plugin URI: http://infoheap.com/
-Description: Include angularjs javascript for a specific post (using custom field angularjs-include)
+Description: Include angularjs javascript for a specific post (using custom field wp-angularjs-include)
 Author: infoheap team
 Author URI: http://infoheap.com/
 Version: 1.0
@@ -11,7 +11,7 @@ Version: 1.0
 
 function waci_enqueue_js () {
   global $post;
-  $post_angularjs_include_value = get_post_meta($post->ID, 'angularjs-include', true);
+  $post_angularjs_include_value = get_post_meta($post->ID, 'wp-angularjs-include', true);
   if (in_array($post_angularjs_include_value, array('true', '1')) ) {
     wp_enqueue_script('angularjs-js', plugins_url('js/angular.js', __FILE__), null, null, true);
   }
